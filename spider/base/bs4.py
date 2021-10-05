674cf9b89774a01c3ac0ec61473d1e96b2e94fd5
wim <m969130721@163.com>
2021-10-06 00:34:39 +0800

    rename

--- spider/base/bs4.py
+++ spider/base/bs4.py
@@ -2,7 +2,7 @@
 # pip install -i https://mirrors.aliyun.com/pypi/simple/ bs4
 #导入
 import requests
-from 爬虫.base.bs4 import BeautifulSoup
+from spider.base.bs4 import BeautifulSoup
 #解析数据
 url = "http://www.baidu.com"
 resp = requests.get(url)
